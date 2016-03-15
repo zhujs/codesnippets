8a7eba74d1aeac8ef432b803f5ce252954eb7488
zhujs <zhujs.w@outlook.com>
2016-03-15 12:26:08 +0800

    Difference between reentrantlock and synchronized

--- java.note/SynchronizedTest.java
+++ java.note/SynchronizedTest.java
@@ -1,3 +1,16 @@
+/*
+ * Difference between synchronized and reentrantlock:
+ *      http://stackoverflow.com/questions/11821801/why-use-a-reentrantlock-if-one-can-use-synchronizedthis
+ *      http://stackoverflow.com/questions/9072422/difference-between-synchronized-and-reentrantlock-pros-and-cons
+ *
+ *      1. reentrantlock supports interruptible lock polling (time-out)
+ *      2. need to wrap lock acquisitions in a try/finally block (not for synchronized)
+ *      3. reentrantLock also supports configurable fairness policy, allowing more flexible thread scheduling.
+ *      4. allow to check for the state of lock (being held or not, the list of waiting threads)
+ *      5. performance difference??
+ *
+*/
+
 public class SynchronizedTest {
     public static void main(String[] args) {
         // Every Java object (including the class object) has a reentrant lock
